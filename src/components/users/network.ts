8b5c0780dfd1375c74afb28a530fc1a6a7ef3cd2
jorgezerpa <jorgezerpacoder@gmail.com>
2022-12-19 15:26:15 -0400

    exclude create user route from auth middleware

--- src/components/users/network.ts
+++ src/components/users/network.ts
@@ -6,16 +6,19 @@ import validatorHandler from '../../middlewares/validator.handler';
 
 const users = express.Router();
 
-users.post('/', validatorHandler(createUserSchema, 'body'), async (req, res, next) => {
-    try{
-        const {name, age} = req.body;
-        const user = {name, age};
-        const postedUser = await postUser(user);
-        successResponse(req, res, postedUser, 201);
-    }catch(error:any){
-        next(error)
-    }
-});
+//TO SCAPE THIS ROUTE TO THE AUTH MIDDLEWARE
+export const createUserClosure = () => {
+    return users.post('/', validatorHandler(createUserSchema, 'body'), async (req, res, next) => {
+        try{
+            const {name, age} = req.body;
+            const user = {name, age};
+            const postedUser = await postUser(user);
+            successResponse(req, res, postedUser, 201);
+        }catch(error:any){
+            next(error)
+        }
+    });
+}
 
 //get users
 users.get('/', async(req, res, next) => {
--- src/network/routes.ts
+++ src/network/routes.ts
@@ -1,11 +1,13 @@
 import { Application } from "express";
 import { checkJwt } from '../libs/auth/auth0'
-
 import messages from '../components/message/network'
 import users from '../components/users/network';
 import chat from '../components/chat/network'
+import { createUserClosure } from "../components/users/network";
 
 export const messageRoute = (server: Application) => {
+    
+    server.use('/users/create', createUserClosure()) //this route from users scape the auth middleware
     server.use(checkJwt)
     server.use('/messages', messages);
     server.use('/users', users);
